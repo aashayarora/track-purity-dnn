5e9ef37ba83febf2292b395b13fc27f4f523a77f
aashayarora <aaarora@ucsd.edu>
2025-11-15 21:20:23 -0800

    revert to patches

--- data/setup_cmssw.sh
+++ data/setup_cmssw.sh
@@ -12,20 +12,13 @@ cmsenv
 git cms-init
 git cms-addpkg HLTrigger/Configuration Configuration/EventContent DQM/Integration DQMOffline/Trigger RecoTracker/FinalTrackSelectors Validation/RecoTrack Configuration/PyReleaseValidation RecoTracker/MkFit
 
-# manos LST PR
-git cherry-pick 4f2c71ec6483e154826d8ac181b62b6e93ad20f3
-
-# Mario mkfit PR
-git cherry-pick c90e64223ae151abca0759388ca305fc129d93cd
-git cherry-pick 332b8f07ed0f4d3fbbbc28b77f08b51dc8e7eec2
+git apply $CURRENT_DIR/prs/mario.patch
+git apply $CURRENT_DIR/prs/manos.patch
 
 cd $CMSSW_BASE/src/RecoTracker/MkFit/
 git clone git@github.com:cms-data/RecoTracker-MkFit.git data
-cd data/
-
-# mario mkfit json PR
-git cherry-pick e19729f499cc7fd6a780922e86252621e892e929
-git cherry-pick c78834a712bac0479fa7739836ed1b50df27f98a
+curl https://raw.githubusercontent.com/cms-data/RecoTracker-MkFit/e19729f499cc7fd6a780922e86252621e892e929/mkfit-phase2-highPtTripletStep.json -o data/mkfit-phase2-highPtTripletStep.json
+curl https://raw.githubusercontent.com/cms-data/RecoTracker-MkFit/e19729f499cc7fd6a780922e86252621e892e929/mkfit-phase2-lstStep.json -o data/mkfit-phase2-lstStep.json
 
 scram b -j 12
 
